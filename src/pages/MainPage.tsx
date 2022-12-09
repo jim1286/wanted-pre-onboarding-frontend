e59044ae666c4e918a8186c7419faedc350ac039
Hwang Jimin <jim1286@khu.ac.kr>
2022-12-10 00:11:05 +0900

    v0.0.0

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../components/login/Login";
+import SignUp from "../components/signup/SignUp";
+
+export default function MainPage() {
+  return (
+    <>
+      <Login />
+      <SignUp />
+    </>
+  );
+}
