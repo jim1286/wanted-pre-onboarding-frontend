9c2ed52461253fef764f345fe20f66e18a8093a2
Hwang Jimin <jim1286@khu.ac.kr>
2022-12-13 19:04:22 +0900

    Refactor : SignUp.tsx 삭제

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,13 +1,11 @@
 import { Navigate } from "react-router-dom";
 import Login from "./Login";
-import SignUp from "./SignUp";
 
 export default function MainPage() {
   return (
     <>
       {localStorage.getItem("token") && <Navigate to="/todo" />}
       <Login />
-      {/* <SignUp /> */}
     </>
   );
 }
